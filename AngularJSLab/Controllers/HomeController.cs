34eebf35d22fcdfa4f851a1f78ef36653716f00c
SkyChang <sancsky@livemail.tw>
2014-11-25 16:37:29 +0800

    教材1 ~ 6

--- AngularJSLab/Controllers/HomeController.cs
+++ AngularJSLab/Controllers/HomeController.cs
@@ -33,5 +33,60 @@ namespace AngularJSLab.Controllers
         {
             return View();
         }
+
+        public ActionResult Lession1()
+        {
+            return View();
+        }
+
+        public ActionResult Lession2()
+        {
+            return View();
+        }
+
+        public ActionResult Lession3()
+        {
+            return View();
+        }
+
+        public ActionResult Lession4()
+        {
+            return View();
+        }
+
+        public ActionResult Lession41()
+        {
+            return View();
+        }
+
+        public ActionResult Lession5()
+        {
+            return View();
+        }
+
+        public ActionResult Lession51()
+        {
+            return View();
+        }
+
+        public ActionResult Lession6()
+        {
+            return View();
+        }
+
+        public ActionResult Lession7()
+        {
+            return View();
+        }
+
+        public ActionResult Lession8()
+        {
+            return View();
+        }
+
+        public ActionResult Lession9()
+        {
+            return View();
+        }
     }
 }
---
+++ AngularJSLab/Scripts/app/event/controllers/eventListCtrl.js
@@ -0,0 +1,28 @@
+﻿angular.module('eventApp', [])
+    .controller('EventListCtrl', function ($scope) {
+
+        //#region Lession 5
+        $scope.Predicate = 'name';
+        $scope.reverse = false;
+        //#endregion
+
+        //#region Lession 5-1
+        $scope.reverses = [
+            { name: '最新在前', shade: true },
+            { name: '最新在後', shade: false }
+        ];
+        $scope.reverseVale = $scope.reverses[0];
+        //#endregion
+
+        $scope.events = [
+          {
+              'date': '2014/11/29',
+              'name': 'AngularJS菁英班 - 上'
+          },
+          {
+              'date': '2014/11/30',
+              'name': 'AngularJS菁英班 - 下'
+          }
+     
+        ];
+});
