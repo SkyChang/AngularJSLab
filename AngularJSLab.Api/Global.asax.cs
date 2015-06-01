cbb367bade145935e6abf9a863e3740c265ce883
Sky Chang <sancsky@livemail.tw>
2015-06-02 00:05:26 +0800

    add CRUD & View

---
+++ AngularJSLab.Api/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using Newtonsoft.Json.Serialization;
+
+namespace AngularJSLab.Api
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            // 讓Web API只回傳JSON格式
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            var jsonformatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            jsonformatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
