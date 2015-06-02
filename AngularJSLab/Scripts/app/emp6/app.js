da210800b9cb9cd994dd5aad6f240f52b923572d
Sky Chang <sancsky@livemail.tw>
2015-06-02 11:35:07 +0800

    add lession 6

---
+++ AngularJSLab/Scripts/app/emp6/app.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+
+var mainApp = angular.module('hrApp', [
+    'ui.router',
+    'hrApp.employeesController',
+    'hrApp.employeesFormController',
+    'hrApp.empService']);
+
+//Route
+mainApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/list');
+
+    $stateProvider
+      .state('list', {
+          url: '/list',
+          templateUrl: '/wwwroot/Crud/lession6List.html',
+          controller: 'employeesController',
+      })
+      .state('add', {
+          url: '/add',
+          templateUrl: '/wwwroot/Crud/lession6Form.html',
+          controller: 'employeesFormController'
+      })
+      .state('edit', {
+          url: '/edit/:eventId',
+          templateUrl: '/wwwroot/Crud/lession6Form.html',
+          controller: 'employeesFormController'
+      })
+}]);
