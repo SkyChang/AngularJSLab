83f147ab1364a7abe65d9cdb481186e44bddc2c7
Sky Chang <sancsky@livemail.tw>
2015-06-08 16:29:27 +0800

    add Directive

---
+++ AngularJSLab/Scripts/app/emp8/app.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+
+var mainApp = angular.module('hrApp', [
+    'ui.router',
+    'hrApp.employeesController',
+    'hrApp.employeesFormController',
+    'hrApp.empService']);
+
+//Route
+mainApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/list');
+
+    $stateProvider
+      .state('list', {
+          url: '/list',
+          templateUrl: '/wwwroot/Crud/lession8List.html',
+          controller: 'employeesController',
+      })
+      .state('add', {
+          url: '/add',
+          templateUrl: '/wwwroot/Crud/lession8Form.html',
+          controller: 'employeesFormController'
+      })
+      .state('edit', {
+          url: '/edit/:eventId',
+          templateUrl: '/wwwroot/Crud/lession8Form.html',
+          controller: 'employeesFormController'
+      })
+}]);
---
+++ AngularJSLab/Scripts/app/emp8/directives/empForms.js
@@ -0,0 +1,36 @@
+﻿'use strict';
+
+//Directive
+var hrAppDirective = angular.module('hrApp.directive', []);
+
+hrAppDirective.directive('empForm', [function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/Scripts/app/emp8/directives/template/empFormView.html',
+        transclude: true,
+        scope: {
+            emp: '=?',
+            onAdd: '&',
+            onUpdate: '&',
+        },
+        link: function (scope, element, attrs) {
+
+            scope.add = function (isValid) {
+                if (!isValid) {
+                    alert('有欄位未填!');
+                    return;
+                }
+
+                scope.onAdd();
+            };
+
+            scope.update = function (id, isValid) {
+                if (!isValid) {
+                    alert('有欄位未填!');
+                    return;
+                }
+                scope.onUpdate({ id : id});
+            }
+        }
+    };
+}]);
