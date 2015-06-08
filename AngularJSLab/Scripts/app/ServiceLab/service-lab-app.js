eb215fae465f8dbf41cc3de3752fab136ca65c42
Sky Chang <sancsky@livemail.tw>
2015-06-08 15:18:39 +0800

    add promise & validation

--- AngularJSLab/Scripts/app/ServiceLab/service-lab-app.js
+++
@@ -1,5 +0,0 @@
-﻿'use strict';
-
-angular.module('serviceLabApp', [
-    'angularJSLab.serviceLab.controllers.serviceLab'
-])
--- AngularJSLab/Scripts/app/event/filter/filterSample.js
+++
@@ -1,6 +0,0 @@
-﻿angular.module('eventFilters', [])
-    .filter('checkmark', function () {
-    return function (input) {
-        return input ? '\u2713' : '\u2718';
-    };
-});
